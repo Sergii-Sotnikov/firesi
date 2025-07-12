5345e8c18e1742758f96136e8f1d18707fd9dba1
SotnikovSS <sotnikovss90@gmail.com>
2025-07-12 23:24:05 +0200

    go

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,17 +5,21 @@ import Fire from "../Fire/Fire";
 import Advantages from "../Advantages/Advantages";
 import Video from "../Video/Video";
 import Solution from "../Solution/Solution";
+import Arguments from "../Arguments/Arguments";
 
 export default function App() {
   return (
     <div className={css.app}>
       <main className={css.main}>
         <Header />
-        <Hero/>
-        <Fire/>
-        <Advantages/>
-        <Video/>
-        <Solution/>
+        <Hero />
+        <section className={css.fireBgWrapper}>
+          <div className={css.fireBg}></div>
+        </section>
+        <Advantages />
+        <Video />
+        <Solution />
+        <Arguments />
       </main>
     </div>
   );
---
+++ src/components/Arguments/Arguments.tsx
@@ -0,0 +1,21 @@
+import css from "./Arguments.module.css"
+
+export default function Arguments(){
+    return(
+        <section className="arguments">
+            <div className="container">
+                <h3 className="title">
+                    чому варто обрати саме  Firesi ?
+                </h3>
+                <ul className="list">
+                    <li className="item">
+                        <p className="number">1</p>
+                        <p className="itemTitle">Вогонь не повернеться! </p>
+                    </li>
+                </ul>
+
+            </div>
+
+        </section>
+    )
+}
