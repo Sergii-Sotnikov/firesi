a5ff8c17a308da9c2584628b91e6c43e8180071b
SotnikovSS <sotnikovss90@gmail.com>
2025-07-15 16:52:02 +0200

    go

--- src/components/Сhoice/Сhoice.tsx
+++ src/components/Сhoice/Сhoice.tsx
@@ -55,6 +55,88 @@ export default function Choice() {
                   </li>
                 </ul>
               </div>
+              <picture className={css.productImage}>
+                <source
+                  srcSet="/img/webp/FRSE_FS.webp 1x, /img/webp/FRSE_FS@2x.webp 2x"
+                  type="image/webp"
+                />
+                <img
+                  src="/img/webp/FRSE_FS.webp"
+                  alt="FRSE FS"
+                />
+              </picture>
+            </div>
+            <div className={css.classes}>
+              <p className={css.classText}>🔥 Гасіння всіх класів пожеж:</p>
+              <p className={css.classText}>
+                🟩 A — тверді речовини (дерево, папір, текстиль)
+              </p>
+              <p className={css.classText}>
+                🟨 B — рідини (бензин, олія, лакофарби)
+              </p>
+              <p className={css.classText}>
+                🟥 C — гази (пропан, метан, бутан)
+              </p>
+              <p className={css.classText}>
+                ⬛ D — метали (магній, натрій, титан)*
+              </p>
+              <p className={css.classText}>
+                🔌 E — електрообладнання під напругою
+              </p>
+              <p className={css.classText}>
+                🍳 F — кухонні жири й масла</p>
+            </div>
+          </li>
+
+          <li className={css.productItem}>
+            <p className={css.productTitle}>ВОГНЕГАСНИК</p>
+            <p className={css.productName}>
+              Fire<span className={css.productNameLogo}>si</span> FRSE-FS
+            </p>
+            <div className={css.product}>
+              <div className={css.productDescription}>
+                <p className={css.descriptionName}>
+                  Спрей: <span className={css.descriptionNameAdd}>FRSE-FS</span>
+                </p>
+                <ul className={css.descriptionList}>
+                  <li className={css.descriptionItem}>
+                    <p className={css.descriptionItemText}>
+                      Тип вогнегасника: водно-пінний
+                    </p>
+                  </li>
+                  <li className={css.descriptionItem}>
+                    <p className={css.descriptionItemText}>
+                      Обʼєм заряду: 400 мл.
+                    </p>
+                  </li>
+                  <li className={css.descriptionItem}>
+                    <p className={css.descriptionItemText}>
+                      Вогнегасна здатність: 34A, 183B, C, D, 75F, Li-ion,
+                      LiFePo4
+                    </p>
+                  </li>
+                  <li className={css.descriptionItem}>
+                    <p className={css.descriptionItemText}>
+                      Для використання на пожежах під напругою до 400В
+                    </p>
+                  </li>
+                  <li className={css.descriptionItem}>
+                    <p className={css.descriptionItemText}>
+                      Можна керувати лише однією рукою
+                    </p>
+                  </li>
+                  <li className={css.descriptionItem}>
+                    <p className={css.descriptionItemText}>
+                      Дальність гасіння 1–2 метри
+                    </p>
+                  </li>
+                  <li className={css.descriptionItem}>
+                    <p className={css.descriptionItemText}>
+                      Температурний діапазон експлуатації: від -15°C
+                    </p>
+                  </li>
+                </ul>
+              </div>
             </div>
             <div className={css.classes}>
               <p className={css.classText}>Гасіння всіх класів пожеж:</p>
@@ -143,79 +225,6 @@ export default function Choice() {
               <p className={css.classText}>F — кухонні жири й масла</p>
             </div>
           </li>
-
-
-          <li className={css.productItem}>
-            <p className={css.productTitle}>ВОГНЕГАСНИК</p>
-            <p className={css.productName}>
-              Fire<span className={css.productNameLogo}>si</span> FRSE-FS
-            </p>
-            <div className={css.product}>
-              <div className={css.productDescription}>
-                <p className={css.descriptionName}>
-                  Спрей: <span className={css.descriptionNameAdd}>FRSE-FS</span>
-                </p>
-                <ul className={css.descriptionList}>
-                  <li className={css.descriptionItem}>
-                    <p className={css.descriptionItemText}>
-                      Тип вогнегасника: водно-пінний
-                    </p>
-                  </li>
-                  <li className={css.descriptionItem}>
-                    <p className={css.descriptionItemText}>
-                      Обʼєм заряду: 400 мл.
-                    </p>
-                  </li>
-                  <li className={css.descriptionItem}>
-                    <p className={css.descriptionItemText}>
-                      Вогнегасна здатність: 34A, 183B, C, D, 75F, Li-ion,
-                      LiFePo4
-                    </p>
-                  </li>
-                  <li className={css.descriptionItem}>
-                    <p className={css.descriptionItemText}>
-                      Для використання на пожежах під напругою до 400В
-                    </p>
-                  </li>
-                  <li className={css.descriptionItem}>
-                    <p className={css.descriptionItemText}>
-                      Можна керувати лише однією рукою
-                    </p>
-                  </li>
-                  <li className={css.descriptionItem}>
-                    <p className={css.descriptionItemText}>
-                      Дальність гасіння 1–2 метри
-                    </p>
-                  </li>
-                  <li className={css.descriptionItem}>
-                    <p className={css.descriptionItemText}>
-                      Температурний діапазон експлуатації: від -15°C
-                    </p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className={css.classes}>
-              <p className={css.classText}>Гасіння всіх класів пожеж:</p>
-              <p className={css.classText}>
-                A — тверді речовини (дерево, папір, текстиль)
-              </p>
-              <p className={css.classText}>
-                B — рідини (бензин, олія, лакофарби)
-              </p>
-              <p className={css.classText}>C — гази (пропан, метан, бутан)</p>
-              <p className={css.classText}>
-                D — метали (магній, натрій, титан)*
-              </p>
-              <p className={css.classText}>
-                E — електрообладнання під напругою
-              </p>
-              <p className={css.classText}>F — кухонні жири й масла</p>
-            </div>
-          </li>
-
-
-          
         </ul>
       </div>
     </section>
