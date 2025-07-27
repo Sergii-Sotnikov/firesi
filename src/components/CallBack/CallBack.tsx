b5088d54efb7c682397a7f6c5d1aa9a748015164
SotnikovSS <sotnikovss90@gmail.com>
2025-07-27 14:01:38 +0200

    go

--- src/components/CallBack/CallBack.tsx
+++ src/components/CallBack/CallBack.tsx
@@ -1,8 +1,19 @@
-
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+import ReCAPTCHA from 'react-google-recaptcha';
+import { PhoneCall } from 'lucide-react';
+import { useRef, useState } from 'react';
+import { Toaster, toast } from 'react-hot-toast';
+import css from "./CallBack.module.css";
 
 export default function Callback(){
-    return(
-        <>
-        </>
-    )
+
+
+return (
+    <>
+      
+    </>
+  );
 }
